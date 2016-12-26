6bc84d0ffd6c5efcf13997c9daa123cbc43afd22
Alex Stokes <r.alex.stokes@gmail.com>
2016-12-26 15:20:01 -0600

    add pretty printer for Lists

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug,Clone)]
 pub enum Ast {
     Symbol(String),
@@ -5,6 +7,91 @@ pub enum Ast {
     List(Vec<Ast>),
     // Combination(PrimOpType, Box<Args>),
     PrimOp(PrimOpType),
+}
+
+impl fmt::Display for Ast {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Ast::Symbol(ref s) => write!(f, "Symbol({})", s),
+            Ast::Number(ref n) => write!(f, "Number({})", n),
+            Ast::List(ref ls) => pretty_print_list(f, ls, 0),
+            Ast::PrimOp(_) => unreachable!(),
+        }
+    }
+}
+
+const SPACER: &'static str = "  ";
+
+fn pretty_print_list(f: &mut fmt::Formatter, ls: &Vec<Ast>, depth: i32) -> fmt::Result {
+    let result = write!(f, "\n");
+    match result {
+        Err(_) => return result,
+        _ => {}
+    }
+    for _ in 0..depth {
+        let result = write!(f, "{}", SPACER);
+        match result {
+            Err(_) => return result,
+            _ => {}
+        }
+    }
+    let result = write!(f, "List([\n");
+    match result {
+        Err(_) => return result,
+        _ => {}
+    }
+    for _ in 0..depth + 1 {
+        let result = write!(f, "{}", SPACER);
+        match result {
+            Err(_) => return result,
+            _ => {}
+        }
+    }
+    for (i, l) in ls.iter().enumerate() {
+        match l {
+            &Ast::List(ref ls) => {
+                let result = pretty_print_list(f, ls, depth + 1);
+                match result {
+                    Err(_) => return result,
+                    _ => {}
+                }
+            }
+            _ => {
+                if i != 0 {
+                    let result = write!(f, " {},", l);
+                    match result {
+                        Err(_) => return result,
+                        _ => {}
+                    }
+                } else {
+                    let result = write!(f, "{},", l);
+                    match result {
+                        Err(_) => return result,
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+    let result = write!(f, "\n");
+    match result {
+        Err(_) => return result,
+        _ => {}
+    }
+    for _ in 0..depth {
+        let result = write!(f, "{}", SPACER);
+        match result {
+            Err(_) => return result,
+            _ => {}
+        }
+    }
+    let result = write!(f, "])");
+    match result {
+        Err(_) => return result,
+        _ => {}
+    }
+    let result = if depth != 0 { write!(f, ",") } else { Ok(()) };
+    result
 }
 
 
