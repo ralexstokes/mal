5a8abb22ead207845a13ce1132c9ddf4000de3db
Alex Stokes <r.alex.stokes@gmail.com>
2016-12-31 13:10:02 -0800

    add load-file prelude fn

--- rust/src/prelude.rs
+++ rust/src/prelude.rs
@@ -2,7 +2,9 @@ use env::Env;
 use repl::{Repl, Error};
 
 pub fn load(repl: &mut Repl, env: Env) -> Result<String, Error> {
-    let inputs = vec!["(def! not (fn* (a) (if a false true)))"];
+    let inputs = vec!["(def! not (fn* (a) (if a false true)))",
+                      "(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \
+                       \")\")))))"];
     for input in inputs {
         try!(repl.rep(input.to_string(), env.clone()));
     }
