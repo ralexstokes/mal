e8ed82fa4645c1755074b78c5d3b015ab700919d
Alex Stokes <r.alex.stokes@gmail.com>
2016-12-31 17:00:36 -0800

    add support for evaluating from file

--- rust/src/prelude.rs
+++ rust/src/prelude.rs
@@ -4,7 +4,8 @@ use repl::{Repl, Error};
 pub fn load(repl: &mut Repl, env: Env) -> Result<String, Error> {
     let inputs = vec!["(def! not (fn* (a) (if a false true)))",
                       "(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \
-                       \")\")))))"];
+                       \")\")))))",
+                      "(def! *ARGV* (list))"];
     for input in inputs {
         try!(repl.rep(input.to_string(), env.clone()));
     }
--- rust/src/repl.rs
+++ rust/src/repl.rs
@@ -4,10 +4,13 @@ use printer::print;
 use eval::eval;
 use env;
 use prelude;
+use types::Ast;
 
 pub struct Repl {
     reader: Reader,
 }
+
+const ARGV_SYMBOL: &'static str = "*ARGV*";
 
 impl Repl {
     pub fn new(prompt: String) -> Repl {
@@ -17,13 +20,38 @@ impl Repl {
     pub fn run(&mut self) {
         let env = env::core();
 
+        let mut pretext: Option<String> = None;
         match prelude::load(self, env.clone()) {
-            Ok(msg) => self.reader.write_ok(msg),
+            Ok(msg) => pretext = msg.clone().into(),
             Err(Error::EmptyOutput) => {}
             Err(Error::EvalError(msg)) => self.reader.write_err(msg),
             Err(Error::EOF) => unreachable!(),
         }
 
+        let args = ::std::env::args().skip(1).collect::<Vec<_>>();
+        if !args.is_empty() {
+            let result = args.split_first()
+                .and_then(|(file_name, env_args)| {
+                    let ast_args = env_args.iter()
+                        .map(|arg| Ast::Symbol(arg.clone()))
+                        .collect::<Vec<_>>();
+                    let list_args = Ast::List(ast_args.to_vec());
+                    env.borrow_mut().set(ARGV_SYMBOL.to_string(), list_args);
+                    self.rep_from_file(file_name, env.clone()).into()
+                });
+            if let Some(result) = result {
+                match result {
+                    Ok(result) => println!("{}", result),
+                    Err(Error::EvalError(msg)) => println!("{}", msg),
+                    _ => {}
+                }
+            }
+            return;
+        }
+
+        if let Some(msg) = pretext {
+            self.reader.write_ok(msg)
+        }
 
         loop {
             match self.rep_from_reader(env.clone()) {
@@ -36,11 +64,14 @@ impl Repl {
     }
 
     fn rep_from_reader(&mut self, env: env::Env) -> Result<String, Error> {
-        let line = self.reader.read();
-        match line {
+        match self.reader.read() {
             Some(line) => self.rep(line, env),
             None => Err(Error::EOF),
         }
+    }
+
+    fn rep_from_file(&mut self, file_name: &str, env: env::Env) -> Result<String, Error> {
+        self.rep(format!("(load-file \"{}\")", file_name), env)
     }
 
     pub fn rep(&mut self, input: String, env: env::Env) -> Result<String, Error> {
