381881225d0249632dcca86d1f4736c64ab0aab6
Alex Stokes <r.alex.stokes@gmail.com>
2017-02-06 21:06:13 -0800

    roll this back to satisfy borrow checker

--- rust/src/eval.rs
+++ rust/src/eval.rs
@@ -12,10 +12,9 @@ pub fn eval(val: LispValue, env: Env) -> EvaluationResult {
             // TODO want to avoid re boxing this
             macroexpand(new_list(seq.clone(), None), env.clone()).and_then(|val| {
                 if let LispType::List(ref seq, ..) = *val {
-                    eval_list(seq.to_vec(), env)
-                } else {
-                    eval(val, env)
+                    return eval_list(seq.to_vec(), env);
                 }
+                eval(val, env)
             })
         }
         LispType::Vector(ref seq, ..) => eval_vector(seq.to_vec(), env),
